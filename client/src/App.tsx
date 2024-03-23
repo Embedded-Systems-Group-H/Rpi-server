3370a625c3393b2ed7b51d3af465fa98dd24f69a
Tung Bui <tungbuidang@gmail.com>
2024-03-23 17:45:02 +0200

    add image to the website. Add some successful run

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Table from './Table';
+import Image from './Lily_run.jpeg';
 
 const Home = () => {
   const [data, setData] = useState<any>(null);
   useEffect(() => {
-    fetch("http://localhost:5000")
+    fetch("http://localhost:5000/")
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((error) => console.log("error while fetching data: ", error));
@@ -13,6 +14,7 @@ const Home = () => {
   
   return (
     <div className="App">
+      <img src={require("./Lily_run.jpg")} width="100%"/>
       {data && data.data && <Table entries={data.data} />}
     </div>
   );
